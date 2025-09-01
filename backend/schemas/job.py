667fc8d5f5fc1e67e9ca33b7e55d17064f81edf8
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-09-01 17:25:48 +0530

    added database config and schemas

---
+++ backend/schemas/job.py
@@ -0,0 +1,23 @@
+from typing import List, Optional, Dict
+from pydantic import BaseModel
+from datetime import datetime
+
+
+
+class StoryJobBase(BaseModel):
+    theme:str
+
+class StoryJobResponse(BaseModel):
+    job_id:int
+    status:str
+    created_at: datetime
+    story_id: Optional[int] = None
+    completed_at: Optional[datetime] = None
+    error: Optional[str] = None
+
+    class Config:
+        from_attributes=True
+
+
+class StoryJobCreate(StoryJobBase):
+    pass
